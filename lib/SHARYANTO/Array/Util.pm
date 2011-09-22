d5973f5ab1d625c9efb6aaa447ebfe8026547469
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2011-09-22 16:49:09 +0700

    Add alias match_regex_or_array, add spec

--- lib/SHARYANTO/Array/Util.pm
+++ lib/SHARYANTO/Array/Util.pm
@@ -3,12 +3,25 @@ package SHARYANTO::Array::Util;
 use 5.010;
 require Exporter;
 our @ISA = qw(Exporter);
-our @EXPORT_OK = qw(match_array_or_regex);
+our @EXPORT_OK = qw(match_array_or_regex match_regex_or_array);
 
 # VERSION
 
-# check whether $needle matches $haystack, haystack maybe an array of values, or
-# a regex.
+our %SPEC;
+
+$SPEC{match_array_or_regex} = {
+    summary => 'Check whether an item matches array of values or regex',
+    args_as => 'array',
+    args => {
+        needle => ["str*" => {
+            arg_pos => 0,
+        }],
+        haystack => ["any*" => {
+            of => ["str*[]*", "str*"], # XXX "str*" should be "regex*"
+            arg_pos => 1,
+        }],
+    },
+};
 sub match_array_or_regex {
     my ($needle, $haystack) = @_;
     my $ref = ref($haystack);
@@ -20,6 +33,9 @@ sub match_array_or_regex {
         die "Can't match when haystack is a $ref";
     }
 }
+
+*match_regex_or_array = \&match_array_or_regex;
+$SPEC{match_regex_or_array} = $SPEC{match_array_or_regex};
 
 1;
 # ABSTRACT: Array-related utilities
