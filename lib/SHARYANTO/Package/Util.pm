d3e6ee8e5f6a7a08093b23ca4d8462b31c13639c
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-03-29 08:34:50 +0700

    Add list_package_contents() & list_subpackages()

--- lib/SHARYANTO/Package/Util.pm
+++ lib/SHARYANTO/Package/Util.pm
@@ -1,12 +1,22 @@
 package SHARYANTO::Package::Util;
 
+use 5.010;
+use strict;
+use warnings;
+
 require Exporter;
 our @ISA = qw(Exporter);
-our @EXPORT_OK = qw(package_exists);
+our @EXPORT_OK = qw(
+                       package_exists
+                       list_package_contents
+                       list_subpackages
+               );
 
 # VERSION
 
 sub package_exists {
+    no strict 'refs';
+
     my $pkg = shift;
 
     return unless $pkg =~ /\A\w+(::\w+)*\z/;
@@ -17,22 +27,84 @@ sub package_exists {
     }
 }
 
+# XXX incomplete/improper
+sub list_package_contents {
+    no strict 'refs';
+
+    my $pkg = shift;
+
+    return () unless !length($pkg) || package_exists($pkg);
+    my $symtbl = \%{$pkg . "::"};
+
+    my %res;
+    while (my ($k, $v) = each %$symtbl) {
+        next if $k =~ /::$/; # subpackage
+        my $n;
+        if (defined *$v{CODE}) {
+            $res{$k} = *$v{CODE}; # subroutine
+            $n++;
+        }
+        if (defined *$v{HASH}) {
+            $res{"\%$k"} = \%{*$v}; # hash
+            $n++;
+        }
+        if (defined *$v{ARRAY}) {
+            $res{"\@$k"} = \@{*$v}; # array
+            $n++;
+        }
+        if (defined(*$v{SCALAR}) # XXX always defined?
+                && defined(${*$v})) { # currently we filter undef values
+            $res{"\$$k"} = \${*$v}; # scalar
+            $n++;
+        }
+
+        if (!$n) {
+            $res{"\*$k"} = $v; # glob
+        }
+    }
+
+    %res;
+}
+
+sub list_subpackages {
+    no strict 'refs';
+
+    my ($pkg, $recursive, $cur_res) = @_;
+
+    return () unless !length($pkg) || package_exists($pkg);
+
+    my $symtbl = \%{$pkg . "::"};
+
+    my $res = $cur_res // [];
+    while (my ($k, $v) = each %$symtbl) {
+        next unless $k =~ s/::$//;
+        my $name = (length($pkg) ? "$pkg\::" : "" ) . $k;
+        push @$res, $name;
+        list_subpackages($name, 1, $res) if $recursive;
+    }
+
+    @$res;
+}
+
 1;
 # ABSTRACT: Package-related utilities
 
 =head1 SYNOPSIS
 
- use SHARYANTO::Package::Util qw(package_exists);
+ use SHARYANTO::Package::Util qw(
+     package_exists
+     list_package_contents
+     list_subpackages
+ );
 
  print "Package Foo::Bar exists" if package_exists("Foo::Bar");
+ my %content   = list_package_contents("Foo::Bar");
+ my @subpkg    = list_subpackages("Foo::Bar");
+ my @allsubpkg = list_subpackages("Foo::Bar", 1); # recursive
 
 
 =head1 DESCRIPTION
 
-
-=head1 FUNCTIONS
-
-None are exported by default, but they can be.
 
 =head2 package_exists($name) => BOOL
 
@@ -42,5 +114,36 @@ table (e.g. C<$Foo::var = 1;> will make the C<Foo> package "exist").
 
 This function can be used e.g. for checking before aliasing one package to
 another. Or to casually check whether a module has been loaded.
+
+=head2 list_package_contents($name) => %res
+
+Return a hash containing package contents. For example:
+
+ (
+     sub1  => \&Foo::Bar::sub1,
+     sub2  => \&Foo::Bar::sub2,
+     '%h1' => \%Foo::Bar::h1,
+     '@a1' => \@Foo::Bar::a1,
+     ...
+ )
+
+This module won't list subpackages. Use list_subpackages() for that.
+
+=head2 list_subpackages($name[, $recursive]) => @res
+
+List subpackages, e.g.:
+
+ (
+     "Foo::Bar::Baz",
+     "Foo::Bar::Qux",
+     ...
+ )
+
+If $recursive is true, will also list subpackages of subpackages, and so on.
+
+
+=head1 SEE ALSO
+
+L<perlmod>
 
 =cut
--- t/package_util.t
+++ t/package_util.t
@@ -4,22 +4,63 @@ use 5.010;
 use strict;
 use warnings;
 
-use SHARYANTO::Package::Util qw(package_exists);
+use SHARYANTO::Package::Util qw(
+                                   package_exists
+                                   list_package_contents
+                                   list_subpackages
+                           );
 use Test::More 0.96;
 
 BEGIN { ok(!package_exists("cps61kDkaNlLTrdXC91"), "package_exists 1"); }
 
 package cps61kDkaNlLTrdXC91;
+
+our $A = 1;
+our @A = ();
+our %A = ();
+our $B = undef;
+sub A  {}
+#our *C;
+
 package main;
 
 ok( package_exists("cps61kDkaNlLTrdXC91"), "package_exists 1b");
 
 package cps61kDkaNlLTrdXC92::cps61kDkaNlLTrdXC93;
+package cps61kDkaNlLTrdXC92::cps61kDkaNlLTrdXC93::cps61kDkaNlLTrdXC94;
 package main;
 
 ok( package_exists("cps61kDkaNlLTrdXC92"), "package_exists 2");
 ok( package_exists("cps61kDkaNlLTrdXC92::cps61kDkaNlLTrdXC93"),
     "package_exists 3");
+
+my %res = list_package_contents("cps61kDkaNlLTrdXC91");
+%res = map {$_ => ref($res{$_})} keys %res;
+#diag explain \%res;
+is_deeply(\%res,
+          {
+              '$A' => 'SCALAR',
+              '%A' => 'HASH',
+              '*B' => '',
+              '@A' => 'ARRAY',
+              'A' => 'CODE'
+          },
+          "list_package_contents 1"
+      );
+
+is_deeply([list_subpackages("cps61kDkaNlLTrdXC92")],
+           ["cps61kDkaNlLTrdXC92::cps61kDkaNlLTrdXC93"],
+           "list_subpackages 1");
+
+is_deeply([list_subpackages("cps61kDkaNlLTrdXC92")],
+           ["cps61kDkaNlLTrdXC92::cps61kDkaNlLTrdXC93"],
+           "list_subpackages 1");
+is_deeply([list_subpackages("cps61kDkaNlLTrdXC92", 1)],
+           [
+               "cps61kDkaNlLTrdXC92::cps61kDkaNlLTrdXC93",
+               "cps61kDkaNlLTrdXC92::cps61kDkaNlLTrdXC93::cps61kDkaNlLTrdXC94",
+           ],
+           "list_subpackages 2");
 
 DONE_TESTING:
 done_testing();
