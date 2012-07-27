d55c520b4bc7b6c89358a35751477ac27051cc15
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-07-27 15:39:27 +0700

    Add module: SHARYANTO::Log::Util

---
+++ lib/SHARYANTO/Log/Util.pm
@@ -0,0 +1,49 @@
+package SHARYANTO::Log::Util;
+
+use 5.010;
+use strict;
+use warnings;
+
+require Exporter;
+our @ISA       = qw(Exporter);
+our @EXPORT_OK = qw(@log_levels $log_levels_re);
+
+# VERSION
+
+our @log_levels = (qw/trace debug info warn error fatal/);
+our $log_levels_re = join("|", @log_levels);
+$log_levels_re = qr/\A(?:$log_levels_re)\z/;
+
+1;
+# ABSTRACT: Log-related utilities
+
+=head1 SYNOPSIS
+
+ use SHARYANTO::Log::Util qw(@log_levels $log_levels_re);
+
+
+=head1 DESCRIPTION
+
+
+=head1 VARIABLES
+
+None are exported by default, but they are exportable.
+
+=head2 @log_levels
+
+Contains log levels, from lowest to highest. Currently these are:
+
+ (qw/trace debug info warn error fatal/)
+
+They can be used as method names to L<Log::Any> ($log->debug, $log->warn, etc).
+
+=head2 $log_levels_re
+
+Contains regular expression to check valid log levels.
+
+
+=head1 SEE ALSO
+
+L<Log::Any>
+
+=cut
---
+++ t/log_util.t
@@ -0,0 +1,18 @@
+#!perl -T
+
+use 5.010;
+use strict;
+use warnings;
+
+use Test::More 0.96;
+
+use SHARYANTO::Log::Util qw(@log_levels $log_levels_re);
+
+ok('warn'  ~~ @log_levels);
+ok('debug' =~ $log_levels_re);
+
+ok('foo'   !~~ @log_levels);
+ok('foo'   !~  $log_levels_re);
+
+DONE_TESTING:
+done_testing();
