933c95fca6b2be24af6c8aef50452fbb4b5a25a4
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2013-04-12 18:52:17 +0700

    Refer to SHARYANTO

--- lib/SHARYANTO/Utils.pm
+++ lib/SHARYANTO/Utils.pm
@@ -11,18 +11,7 @@ __END__
 =head2 What is this?
 
 This distribution is a heterogenous collection of modules that will eventually
-have their own proper distributions, but do not yet because they are not ready
-for some reason or another. For example: alpha quality code, code not yet
-properly refactored, there are still no tests and/or documentation, I haven't
-decided on a proper name yet, etc.
-
-I put it on CPAN because some of my other modules (and scripts) depend on it.
-And I always like to put as much of my code in functions and modules (as opposed
-to scripts) as possible, for better reusability.
-
-You are free to use this, but beware that things might get moved around without
-prior warning.
-
-I assure you that this is not a vanity distribution :-)
+have their own proper distributions. See L<SHARYANTO> about this temporary
+namespace.
 
 =cut
