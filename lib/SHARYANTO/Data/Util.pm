85e4d22ab92d2b6b5432f3941be473ea4c810f61
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2013-07-11 18:57:29 +0700

    Add module: SHARYANTO::Data::Util

---
+++ lib/SHARYANTO/Data/Util.pm
@@ -0,0 +1,80 @@
+package SHARYANTO::Data::Util;
+
+use 5.010;
+use strict;
+use warnings;
+#use experimental 'smartmatch';
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(has_circular_ref);
+
+# VERSION
+
+our %SPEC;
+
+$SPEC{has_circular_ref} = {
+    v => 1.1,
+    summary => 'Check whether data item contains circular references',
+    description => <<'_',
+
+Does not deal with weak references.
+
+_
+    args_as => 'array',
+    args => {
+        data => {
+            schema => "any",
+            pos => 0,
+            req => 1,
+        },
+    },
+    result_naked => 1,
+};
+sub has_circular_ref {
+    my ($data) = @_;
+    my %refs;
+    my $check;
+    $check = sub {
+        my $x = shift;
+        my $r = ref($x);
+        return 0 if !$r;
+        return 1 if $refs{$r}++;
+        if ($r eq 'ARRAY') {
+            for (@$x) {
+                next unless ref($_);
+                return 1 if $check->($_);
+            }
+        } elsif ($r eq 'HASH') {
+            for (values %$x) {
+                next unless ref($_);
+                return 1 if $check->($_);
+            }
+        }
+        0;
+    };
+    $check->($data);
+}
+
+1;
+# ABSTRACT: Data utilities
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+
+=head1 FUNCTIONS
+
+None are exported by default, but they are exportable.
+
+
+=head1 SEE ALSO
+
+For checking circular references, use C<has_circular_ref> from
+L<Data::Structure::Util> which is the XS version (3 times or more faster than
+this module's implementation which is pure Perl). This module is however much
+faster than L<Devel::Cycle>.
+
+=cut
