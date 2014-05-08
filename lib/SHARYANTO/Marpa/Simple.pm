46f81cba8861df38a27465aba744b798a64c41ee
Steven Haryanto (on Asus Zenbook) <stevenharyanto@gmail.com>
2014-05-08 16:38:49 +0700

    Add module: Marpa::Simple

---
+++ lib/SHARYANTO/Marpa/Simple.pm
@@ -0,0 +1,103 @@
+package SHARYANTO::Marpa::Simple;
+
+use Marpa::R2;
+use UUID::Random;
+
+use Exporter qw(import);
+our @EXPORT_OK = qw(gen_parser);
+
+# VERSION
+# DATE
+
+our %SPEC = {
+    v => 1.1,
+    summary => 'Generate Marpa-based parser',
+    args => {
+        grammar => {
+            summary => '',
+            schema  => 'str*',
+            req => 1,
+            pos => 0,
+        },
+        actions => {
+            summary => '',
+            schema  => ['hash*', each_value => 'code*'],
+        },
+    },
+    result_naked => 1,
+    result => {
+        schema => 'code*',
+    },
+};
+sub gen_parser {
+    no strict 'refs';
+
+    my %args = @_;
+
+    my $grammar = Marpa::R2::Scanless::G->new({source => \$args{grammar} });
+    my $pkg = __PACKAGE__ . '::gen' . substr(UUID::Random::generate(), 0, 8);
+    my $acts = $args{actions};
+    for (keys %$acts) {
+        *{"$pkg\::$_"} = $acts->{$_};
+    }
+
+    my $parser = sub {
+        my $input = shift;
+
+        my $recce = Marpa::R2::Scanless::R->new({
+            grammar => $grammar,
+            semantics_package => $pkg,
+        });
+        $recce->read(\$input);
+        my $valref = $recce->value;
+        if (!defined($valref)) {
+            die "No parse was found after reading the entire input\n";
+            # XXX show last expression
+        }
+        $$valref;
+    };
+
+    $parser;
+}
+
+1;
+# ABSTRACT: Generate Marpa-based parser
+
+=head1 SYNOPSIS
+
+ use SHARYANTO::Marpa::Simple qw(gen_parser);
+
+ my $parser = gen_parser(
+     grammar => <<'EOG',
+ :start     ::= expr
+ expr       ::= num
+              | num '+' num    action => do_add
+ num        ~ [\d]+
+ :discard   ~ whitespace
+ whitespace ~ [\s]+
+ EOG
+     actions => {
+         do_add => sub { shift; $_[0] + $_[2] }
+     },
+ );
+
+ print $parser->('3 + 4'); # -> 7
+ print $parser->('3 + ');  # dies with parse error
+
+which is a shortcut for roughly this:
+
+ no strict 'refs';
+ use Marpa::R2;
+ my $grammar = Marpa::R2::Scanless::G->new({source => \$args{grammar}});
+ my $pkg = "SHARYANTO::Marpa::Simple::gen" . some_random_value();
+ my $actions = $args{actions};
+ for (keys %$actions) {
+     ${"$pkg\::$_"} = $actions->{$_};
+ }
+ my $parser = sub {
+     my $input = shift;
+     my $recce = Marpa::R2::Scanless::R->new({
+         grammar => $grammar,
+         semantics_package => $pkg,
+     });
+ };
