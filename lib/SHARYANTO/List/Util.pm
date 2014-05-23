03753894db4f38a49f21148f31b3cb2213b42f6b
Steven Haryanto (on PC, Bandung) <stevenharyanto@gmail.com>
2014-05-23 13:14:08 +0700

    Move functions find_missing_{nums,strs}_in_seq() from SHARYANTO::Number::Util to SHARYANTO::List::Util

--- lib/SHARYANTO/List/Util.pm
+++ lib/SHARYANTO/List/Util.pm
@@ -8,6 +8,8 @@ require Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT_OK = qw(
                        uniq
+                       find_missing_nums_in_seq
+                       find_missing_strs_in_seq
                );
 
 # VERSION
@@ -29,25 +31,60 @@ sub uniq {
     @res;
 }
 
+sub find_missing_nums_in_seq {
+    require List::Util;
+
+    my @res;
+    my $min = List::Util::min(@_);
+    my $max = List::Util::max(@_);
+
+    my %h = map { $_=>1 } @_;
+    for ($min..$max) {
+        push @res, $_ unless $h{$_};
+    }
+    wantarray ? @res : \@res;
+}
+
+sub find_missing_strs_in_seq {
+    require List::Util;
+
+    my @res;
+    my $min = List::Util::minstr(@_);
+    my $max = List::Util::maxstr(@_);
+
+    my %h = map { $_=>1 } @_;
+    for ($min..$max) {
+        push @res, $_ unless $h{$_};
+    }
+    wantarray ? @res : \@res;
+}
+
 1;
 # ABSTRACT: List utilities
 
-=head1 SYNOPSIS
-
- use SHARYANTO::List::Util qw(uniq);
-
- my @res = uniq(1, 4, 4, 3, 1, 1, 2); # 1, 4, 3, 1, 2
-
-
-=head1 DESCRIPTION
-
-
 =head1 FUNCTIONS
+
+Not exported by default but exportable.
 
 =head2 uniq(@list) => LIST
 
 Remove I<adjacent> duplicates from list, i.e. behave more like Unix utility's
-B<uniq> instead of L<List::MoreUtils>'s C<uniq> function.
+B<uniq> instead of L<List::MoreUtils>'s C<uniq> function, e.g.
+
+ my @res = uniq(1, 4, 4, 3, 1, 1, 2); # 1, 4, 3, 1, 2
+
+=head2 find_missing_nums_in_seq(LIST) => LIST
+
+Given a list of integers, return number(s) missing in the sequence, e.g.:
+
+ find_missing_nums_in_seq(1, 2, 3, 4, 7, 8); # (5, 6)
+
+=head2 find_missing_strs_in_seq(LIST) => LIST
+
+Like C<find_missing_nums_in_seq>, but for strings/letters "a".."z".
+
+ find_missing_strs_in_seq("a", "e", "b"); # ("c", "d")
+ find_missing_strs_in_seq("aa".."zu", "zz"); # ("zv", "zw", "zx", "zy")
 
 
 =head1 SEE ALSO
--- t/list_util.t
+++ t/list_util.t
@@ -8,10 +8,20 @@ use Test::More 0.98;
 
 use SHARYANTO::List::Util qw(
                                 uniq
+                                find_missing_nums_in_seq
+                                find_missing_strs_in_seq
                         );
 
 subtest "uniq" => sub {
     is_deeply([uniq(1, 2, 4, 4, 4, 2, 4)], [1, 2, 4, 2, 4]);
+};
+
+subtest find_missing_nums_in_seq => sub {
+    is_deeply([find_missing_nums_in_seq(1, 1, 3, 4, 6, 8, 7)], [2, 5]);
+};
+
+subtest find_missing_strs_in_seq => sub {
+    is_deeply([find_missing_strs_in_seq("a", "c", "e")], ["b", "d"]);
 };
 
 DONE_TESTING:
