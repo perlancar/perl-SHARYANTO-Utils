77fc6c5923646b18b4ee3c43d951cc52ab96ba99
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2017-12-09 18:33:37 +0700

    Move SHARYANTO::Array:Util's match_array_or_regex/match_regex_or_array to String-Util-Match

--- lib/SHARYANTO/Array/Util.pm
+++ lib/SHARYANTO/Array/Util.pm
@@ -13,8 +13,6 @@ use Perinci::Sub::Util qw(gen_modified_sub);
 require Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT_OK = qw(
-                       match_array_or_regex
-                       match_regex_or_array
                        split_array
                        replace_array_content
                );
@@ -25,72 +23,6 @@ $SPEC{':package'} = {
     v => 1.1,
     summary => 'Array-related utilities',
 };
-
-my $_str_or_re = ['any*'=>{of=>['re*','str*']}];
-
-$SPEC{match_array_or_regex} = {
-    v => 1.1,
-    summary => 'Check whether an item matches (list of) values/regexes',
-    description => <<'_',
-
-This routine can be used to match an item against a regex or a list of
-strings/regexes, e.g. when matching against an ACL.
-
-Since the smartmatch (`~~`) operator can already match against a list of strings
-or regexes, this function is currently basically equivalent to:
-
-    if (ref($haystack) eq 'ARRAY') {
-        return $needle ~~ @$haystack;
-    } else {
-        return $needle =~ /$haystack/;
-    }
-
-_
-    examples => [
-        {args=>{needle=>"abc", haystack=>["abc", "abd"]}, result=>1},
-        {args=>{needle=>"abc", haystack=>qr/ab./}, result=>1},
-        {args=>{needle=>"abc", haystack=>[qr/ab./, "abd"]}, result=>1},
-    ],
-    args_as => 'array',
-    args => {
-        needle => {
-            schema => ["str*"],
-            pos => 0,
-            req => 1,
-        },
-        haystack => {
-            # XXX checking this schema might actually take longer than matching
-            # the needle! so when arg validation is implemented, provide a way
-            # to skip validating this schema
-
-            schema => ["any*" => {
-                of => [$_str_or_re, ["array*"=>{of=>$_str_or_re}]],
-            }],
-            pos => 1,
-            req => 1,
-        },
-    },
-    result_naked => 1,
-};
-sub match_array_or_regex {
-    my ($needle, $haystack) = @_;
-    my $ref = ref($haystack);
-    if ($ref eq 'ARRAY') {
-        return $needle ~~ @$haystack;
-    } elsif (!$ref) {
-        return $needle =~ /$haystack/;
-    } elsif ($ref eq 'Regexp') {
-        return $needle =~ $haystack;
-    } else {
-        die "Invalid haystack, must be regex or array of strings/regexes";
-    }
-}
-
-gen_modified_sub(
-    output_name => 'match_regex_or_array',
-    base_name   => 'match_array_or_regex',
-    summary     => 'Alias for match_array_or_regex',
-);
 
 sub split_array {
     no strict 'refs';
@@ -138,11 +70,7 @@ sub replace_array_content {
 
 =head1 SYNOPSIS
 
- use SHARYANTO::Array::Util qw(match_array_or_regex split_array);
-
- match_array_or_regex('bar',  ['foo', 'bar', qr/[xyz]/]); # true, matches string
- match_array_or_regex('baz',  ['foo', 'bar', qr/[xyz]/]); # true, matches regex
- match_array_or_regex('oops', ['foo', 'bar', qr/[xyz]/]); # false
+ use SHARYANTO::Array::Util qw(split_array);
 
  my @res = split_array('--', [qw/--opt1 --opt2 -- foo bar -- --val/]);
  # -> ([qw/--opt1 --opt2/],  [qw/foo bar/],  [qw/--val/])
